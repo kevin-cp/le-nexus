d0779a5f90a2ad70f7cfcb1182f2e627f4f8554d
Tatatarjaika <fdzuidzia@gmail.com>
2021-08-24 10:04:39 +0200

    save before production test

--- src/middlewares/loginMiddleware.js
+++ src/middlewares/loginMiddleware.js
@@ -1,7 +1,6 @@
 /* eslint-disable default-case */
 import axios from 'axios';
 
-
 import { SUBMIT_LOGIN, getUserData, GET_USER_DATA, changePseudo, changeSteamId, changeSteamAvatar, changeSteamUsername, changeToken, changeVisibilityState, isLogged } from '../actions/login';
 
 const loginMiddleware = (store) => (next) => (action) => {
@@ -29,33 +28,48 @@ const loginMiddleware = (store) => (next) => (action) => {
           // store.dispatch(userLogged());
           // requête des récupérations de données
           store.dispatch(changeToken(response.data.token));
+          store.dispatch(changeSteamId(response.data.authenticatedUserId));
           store.dispatch(getUserData());
         })
 
         .catch((error) => {
           console.log(error);
+          console.log(error.response);
+          console.log(error.response.data);
+          console.log(error.response.status);
+          console.log(error.response.headers);
+
+          if (error.response.data.message == 'Invalid credentials.') {
+            alert('Identifiants invalides');
+          }
         });
       break;
     }
     //! REQUETE QUI RECUPERE LES DONNEES DU USER
-    case GET_USER_DATA:
+    case GET_USER_DATA: {
+      const {
+        steamId,
+      } = store.getState().homepage;
+
       axios.get(
-        'http://localhost:8000/api/users/76561197974266514'
+        `http://localhost:8000/api/users/${steamId}`,
       )
         .then((response)=> {
           console.log(response);
           // Maintenant il faut appeler toutes les fonctions qui modifient le state
           store.dispatch(changePseudo(response.data.pseudo));
-          store.dispatch(changeSteamId(response.data.steamId));
           store.dispatch(changeSteamAvatar(response.data.steamAvatar));
           store.dispatch(changeSteamUsername(response.data.steamUsername));
           store.dispatch(changeVisibilityState(response.data.visibilityState));
           store.dispatch(isLogged());
+          // TODO Redirection une fois logged in
+
         })
         .catch((error) =>{
           console.log(error);
         });
       break;
+    }
   }
 
   // on passe l'action au suivant (middleware suivant ou reducer)
