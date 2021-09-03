70a19831f3c58881391e9a17712bc951a82dbef5
Tatatarjaika <fdzuidzia@gmail.com>
2021-09-03 10:02:59 +0200

    Team page done

--- src/components/Team/index.js
+++ src/components/Team/index.js
@@ -11,6 +11,10 @@ import { Link } from 'react-router-dom';
 import './team.scss';
 
 import Tatayoyoavatar from 'src/assets/images/devteam/Tatayoyo.jpg';
+import Armandavatar from 'src/assets/images/devteam/Armand.jpg';
+import Julienavatar from 'src/assets/images/devteam/Julien.png';
+import Kevinavatar from 'src/assets/images/devteam/Kevin.jpg';
+import Florianavatar from 'src/assets/images/devteam/Florian.jpg';
 
 const Team = () => (
   <Grid className="team-container" centered stackable style={{ height: '100vh', overflow: 'auto' }} divided>
@@ -34,29 +38,29 @@ const Team = () => (
       </Grid.Column>
       <Grid.Column width={4} className="team-developper">
         <Card>
-          <Image src={Tatayoyoavatar} size='medium' />
+          <Image src={Kevinavatar} size='medium' />
           <Card.Content>
-            <Card.Header>François Dzuidzia</Card.Header>
-            <Card.Meta><Icon name="steam" color="black" /> : Tatayoyo </Card.Meta>
-            <Card.Description>Front Developper - Product Owner</Card.Description>
+            <Card.Header>Kevin Capitao Peixoto</Card.Header>
+            <Card.Meta><Icon name="steam" color="black" /> : t3sseract </Card.Meta>
+            <Card.Description>Front Developper - Lead Dev Front</Card.Description>
           </Card.Content>
           <Card.Content>
           <Card.Meta>Jeux favoris:</Card.Meta>
-          <Card.Description>Stellaris, Half-life, Cyberpunk 2077, Amnesia, Léa Passion Cheval</Card.Description>
+          <Card.Description>Half-Life 2, Silent HIll 2, Skyrim, Fallout: New Vegas, Resident Evil 4</Card.Description>
           </Card.Content>
         </Card>
       </Grid.Column>
       <Grid.Column width={4} className="team-developper">
         <Card>
-          <Image src={Tatayoyoavatar} size='medium' />
+          <Image src={Armandavatar} size='medium' />
           <Card.Content>
-            <Card.Header>François Dzuidzia</Card.Header>
-            <Card.Meta><Icon name="steam" color="black" /> : Tatayoyo </Card.Meta>
-            <Card.Description>Front Developper - Product Owner</Card.Description>
+            <Card.Header>Armand Ramière</Card.Header>
+            <Card.Meta><Icon name="steam" color="black" /> : armandramiere </Card.Meta>
+            <Card.Description>Front Developper - Scrum Master</Card.Description>
           </Card.Content>
           <Card.Content>
           <Card.Meta>Jeux favoris:</Card.Meta>
-          <Card.Description>Stellaris, Half-life, Cyberpunk 2077, Amnesia, Léa Passion Cheval</Card.Description>
+          <Card.Description>DOOM, Skyrim, Red Dead Redemption, Mass Effect, Ace Combat </Card.Description>
           </Card.Content>
         </Card>
       </Grid.Column>
@@ -64,29 +68,29 @@ const Team = () => (
     <Grid.Row columns={2}>
     <Grid.Column width={4} className="team-developper">
         <Card>
-          <Image src={Tatayoyoavatar} size='medium' />
+          <Image src={Florianavatar} size='medium' />
           <Card.Content>
-            <Card.Header>François Dzuidzia</Card.Header>
-            <Card.Meta><Icon name="steam" color="black" /> : Tatayoyo </Card.Meta>
-            <Card.Description>Front Developper - Product Owner</Card.Description>
+            <Card.Header>Florian Langard</Card.Header>
+            <Card.Meta><Icon name="steam" color="black" /> : flandwich </Card.Meta>
+            <Card.Description>Back Developper - Git Master</Card.Description>
           </Card.Content>
           <Card.Content>
           <Card.Meta>Jeux favoris:</Card.Meta>
-          <Card.Description>Stellaris, Half-life, Cyberpunk 2077, Amnesia, Léa Passion Cheval</Card.Description>
+          <Card.Description>No Man's Sky, No Man's Sky, No Man's Sky, No Man's Sky, Star Citizen</Card.Description>
           </Card.Content>
         </Card>
       </Grid.Column>
       <Grid.Column width={4} className="team-developper">
         <Card>
-          <Image src={Tatayoyoavatar} size='medium' />
+          <Image src={Julienavatar} size='medium' />
           <Card.Content>
-            <Card.Header>François Dzuidzia</Card.Header>
-            <Card.Meta><Icon name="steam" color="black" /> : Tatayoyo </Card.Meta>
-            <Card.Description>Front Developper - Product Owner</Card.Description>
+            <Card.Header>Julien Gassman</Card.Header>
+            <Card.Meta><Icon name="steam" color="black" /> : Je suis un intrus, j'ai pas de pc </Card.Meta>
+            <Card.Description>Back Developper - Lead Dev Back</Card.Description>
           </Card.Content>
           <Card.Content>
           <Card.Meta>Jeux favoris:</Card.Meta>
-          <Card.Description>Stellaris, Half-life, Cyberpunk 2077, Amnesia, Léa Passion Cheval</Card.Description>
+          <Card.Description>Nier: Automata, Horizon: Zero Dawn, Nintendogs, Apex Legends, Monopoly: Mario Kart Edition</Card.Description>
           </Card.Content>
         </Card>
       </Grid.Column>
